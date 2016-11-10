b150cf5976a46ff37528a820a1d434dcb21b864c
SirRade <jn_ferner@hotmail.de>
2016-11-11 00:57:32 +0100

    Add harambe

--- NEAT/neatmain.cpp
+++ NEAT/neatmain.cpp
@@ -130,7 +130,7 @@ int main(int argc, char *argv[]) {
 
   if (p)
     delete p;
-
+  cin >> choice;
   return(0);
  
 }
--- NEAT/params256.ne
+++ NEAT/params256.ne
@@ -30,4 +30,4 @@ dropoff_age 1000
 newlink_tries 20
 print_every 20
 babies_stolen 0
-
+num_runs 1
